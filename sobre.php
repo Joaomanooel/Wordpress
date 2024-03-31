43a543ac301ff95a525eb532de0c7a2fab05a2aa
João Manoel <Jdarocha206@gmail.com>
2024-03-31 01:44:14 -0300

    Pagina Sobre
    Foi adicionado a pagina sobre

---
+++ sobre.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Danki Code - Tema</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+	<section class="topo">
+		<div class="center">
+			<header>
+				<div class="logo"><img src="images/logo/logo.png" /></div><!--logo-->
+			</header>
+			<ul class="menu-desktop">
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+			<div class="menu-mobile">
+			<i class="fas fa-align-right"></i>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div><!--menu-mobile-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--topo-->
+
+	<section class="sobre-equipe">
+		<div class="center">
+			<div class="w50 equipe-sobre-texto">
+				<h2>Uma equipe estrategica para um trabalho incrivel</h2>
+				<p> É chegada uma nova era, o tempo de recomeçar a rever conceitos e valores. O ser humano é extremamente dependente dos seus semelhantes, pois nenhuma pessoa consegue viver sem depender de ninguém. O calçado, a camisa, o alimento, tudo de que necessitamos teve o trabalho e a mão de obra de uma outra pessoa que muitas vezes nem conhecemos, mas, de certa forma, essa pessoa nos favoreceu...</p>
+			</div>
+			<div class="w50">
+				<img src="images/sobre/equipe.png">
+			</div>
+			<div class="clear"></div>
+		</div>
+	</section>
+</body>
+</html>
